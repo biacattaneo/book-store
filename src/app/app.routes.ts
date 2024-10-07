bea70fe391f901ed6426169922edc9347de17ebc
biacattaneo <biacattaneo2013@gmail.com>
2024-10-07 14:28:21 -0300

    feat: create link and page create-books

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { CreateBookComponent } from './features/create-book/create-book.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
   path: '',
   component: ListComponent
-}];
+},
+  {
+    path: 'create-book',
+    component: CreateBookComponent
+  }
+];
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { BooksService } from '../../shared/services/books.service';
 import type { Books } from '../../shared/interfaces/books.interface';
 import { CardComponent } from './components/card/card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
